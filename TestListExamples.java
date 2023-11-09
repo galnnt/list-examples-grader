7463ccfd08d4e39fd45974931ab390fced561f9d
gallant <ctsao@ucsd.edu>
2023-11-08 16:06:17 -0800

    Bash

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,4 +20,24 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+    public void testMerge() {
+        List<String> strl1 = new ArrayList<>();
+        List<String> strl2 = new ArrayList<>();
+        List<String> strl3 = new ArrayList<>();
+        strl1.add("a");
+        strl1.add("c");
+        strl1.add("e");
+        strl2.add("b");
+        strl2.add("d");
+        strl2.add("f");
+        strl3.add("a");
+        strl3.add("b");
+        strl3.add("c");
+        strl3.add("d");
+        strl3.add("e");
+        strl3.add("f");
+        assertEquals(strl3, ListExamples.merge(strl1, strl2));
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,13 +1,40 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
+# 1. Cloning the student's repo
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# 2. Checks if the file is in the repo
+set -e
+if ! [[ -f ./student-submission/ListExamples.java ]]
+then
+echo 'ListExamples.java not found in the directory! Check the file location :)'
+exit 0
+fi
+
+# 3. copies files into the grading area 
+cp -r student-submission/ grading-area
+cp TestListExamples.java grading-area
+
+# 4. Compile the tests and the student's code
+set +e
+cd ./grading-area
+javac -cp $CPATH *.java
+if [[ $? != 0 ]]
+then
+echo 'Compilation error!!!'
+exit
+fi
+
+# 5. Run the tests and report the grade
+set -e
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > grade.txt
+grep -e "OK" >> grade.txt
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
